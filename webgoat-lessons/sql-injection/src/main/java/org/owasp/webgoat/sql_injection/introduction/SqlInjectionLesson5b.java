b062de4e33b21f22da8f596f2648f0cd9b0d5319
CxJasonT <89665001+CxJasonT@users.noreply.github.com>
2023-07-18 14:39:36 -0400

    Update SqlInjectionLesson5b.java

--- webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/sql_injection/introduction/SqlInjectionLesson5b.java
+++ webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/sql_injection/introduction/SqlInjectionLesson5b.java
@@ -53,11 +53,13 @@ public class SqlInjectionLesson5b extends AssignmentEndpoint {
     }
 
     protected AttackResult injectableQuery(String login_count, String accountName) {
-        String queryString = "SELECT * From user_data WHERE Login_Count = ? and userid= " + accountName;
+        //String queryString = "SELECT * From user_data WHERE Login_Count = ? and userid= " + accountName;
+        String queryString = "SELECT * From user_data WHERE Login_Count = ? and userid= ?";
         try (Connection connection = dataSource.getConnection()) {
             PreparedStatement query = connection.prepareStatement(queryString, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
             int count = 0;
+            string secureAccountName = "";
             try {
                 count = Integer.parseInt(login_count);
             } catch (Exception e) {
@@ -66,6 +68,7 @@ public class SqlInjectionLesson5b extends AssignmentEndpoint {
             }
 
             query.setInt(1, count);
+            query.setString(2,accountNAme);
             //String query = "SELECT * FROM user_data WHERE Login_Count = " + login_count + " and userid = " + accountName, ;
             try {
                 ResultSet results = query.executeQuery();
